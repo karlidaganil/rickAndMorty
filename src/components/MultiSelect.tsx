8ddb591f46088566dd7ecb4c8270335ade9e0a75
Anıl Karlıdağ <anil.karlidag@globit.com.tr>
2024-01-04 14:53:02 +0300

    Add character details and update multi-select functionality

--- src/components/MultiSelect.tsx
+++ src/components/MultiSelect.tsx
@@ -3,19 +3,100 @@ import "./style.scss";
 
 const options = [
   {
-    text: "Rick Sanches",
+    id: 1,
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
+    origin: {
+      name: "Earth",
+      url: "https://rickandmortyapi.com/api/location/1",
+    },
+    location: {
+      name: "Earth",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    episode: [
+      "https://rickandmortyapi.com/api/episode/1",
+      "https://rickandmortyapi.com/api/episode/2",
+      // ...
+    ],
+    url: "https://rickandmortyapi.com/api/character/1",
+    created: "2017-11-04T18:48:46.250Z",
   },
   {
-    text: "Morty Smith",
+    id: 2,
+    name: "Morty Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
+    origin: {
+      name: "Earth",
+      url: "https://rickandmortyapi.com/api/location/1",
+    },
+    location: {
+      name: "Earth",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    episode: [
+      "https://rickandmortyapi.com/api/episode/1",
+      "https://rickandmortyapi.com/api/episode/2",
+      // ...
+    ],
+    url: "https://rickandmortyapi.com/api/character/2",
+    created: "2017-11-04T18:50:21.651Z",
   },
   {
-    text: "Summer Smith",
+    id: 3,
+    name: "Anıl Karlıdağ",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
+    origin: {
+      name: "Earth",
+      url: "https://rickandmortyapi.com/api/location/1",
+    },
+    location: {
+      name: "Earth",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    episode: [
+      "https://rickandmortyapi.com/api/episode/1",
+      "https://rickandmortyapi.com/api/episode/2",
+      // ...
+    ],
+    url: "https://rickandmortyapi.com/api/character/1",
+    created: "2017-11-04T18:48:46.250Z",
   },
   {
-    text: "Beth Smith",
-  },
-  {
-    text: "Jerry Smith",
+    id: 4,
+    name: "Şebnem Karlıdağ",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
+    origin: {
+      name: "Earth",
+      url: "https://rickandmortyapi.com/api/location/1",
+    },
+    location: {
+      name: "Earth",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    episode: [
+      "https://rickandmortyapi.com/api/episode/1",
+      "https://rickandmortyapi.com/api/episode/2",
+      // ...
+    ],
+    url: "https://rickandmortyapi.com/api/character/2",
+    created: "2017-11-04T18:50:21.651Z",
   },
 ];
 
@@ -29,7 +110,14 @@ const MultiSelect = () => {
         {selectedItems.map((item) => (
           <button>
             <span>{item}</span>
-            <span className="delete-icon">x</span>
+            <span
+              className="delete-icon"
+              onClick={() => {
+                setSelectedItems((prev) => prev.filter((i) => i !== item));
+              }}
+            >
+              x
+            </span>
           </button>
         ))}
         <input type="text" placeholder="Please Select Character" />
@@ -39,18 +127,19 @@ const MultiSelect = () => {
           <div>
             <input
               type="checkbox"
-              value={item.text}
+              checked={selectedItems.includes(item.name)}
+              //   value={item.name}
               onChange={(e) => {
                 if (e.target.checked) {
-                  setSelectedItems((prev) => [...prev, item.text]);
+                  setSelectedItems((prev) => [...prev, item.name]);
                 } else {
                   setSelectedItems((prev) =>
-                    prev.filter((i) => i !== item.text)
+                    prev.filter((i) => i !== item.name)
                   );
                 }
               }}
             />
-            {item.text}
+            {item.name}
           </div>
         ))}
       </div>
